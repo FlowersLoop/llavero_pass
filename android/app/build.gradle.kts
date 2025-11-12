d34f4531bb1001f7d1f9276304c3930875c84f44
FlowersLoop <fernandofl0505@gmail.com>
2025-11-12 16:57:41 -0600

    chore(android): migrate build config to Java 17 (compileOptions + kotlinOptions)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,16 +7,17 @@ plugins {
 
 android {
     namespace = "com.ibero.llavero_pass"
+    defaultConfig { applicationId = "com.ibero.llavero_pass" }
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
