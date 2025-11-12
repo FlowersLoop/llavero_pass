5b14575be5a96b38f1d85fb7a63723b3c4ccf6c4
FlowersLoop <fernandofl0505@gmail.com>
2025-11-12 13:41:31 -0600

    test: replace Flutter template test that referenced non-existent MyApp

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+// test/widget_test.dart
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('placeholder', () {
+    // Simple sanity check para mantener el test runner activo.
+    expect(2 + 2, equals(4));
+  });
+}
